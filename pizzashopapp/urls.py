77a502e531b29a5be8ec9ce757924db2821d7b6c
Dmitry <kiselevdv1991@gmail.com>
2020-12-06 11:34:49 +0300

    feat: add login/logout

--- pizzashopapp/urls.py
+++ pizzashopapp/urls.py
@@ -1,6 +1,20 @@
 from django.conf.urls import url
+# Для реализации системы входа/выхода (login/logout) - auth_views
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+
+    # АУТЕНТИФИКАЦИЯ
+    # Авторизация самого владельца и его пиццерии
+    url(r'^pizzashop/sing-in/$', auth_views.login,
+        {'template_name': 'pizzashop/sing_in.html'},
+        name='pizzashop-sing-in'),
+    url(r'^pizzashop/sing-out/$', auth_views.logout,
+        {'next_page': '/'},
+        name='pizzashop-sing-out'),
+    url(r'^pizzashop/$', views.pizzashop_home, name='pizzashop-home'),
+    # Регистрация самого владельца и его пиццерии
+    url(r'^pizzashop/sing-up/$', views.pizzashop_sing_up, name='pizzashop-sing-up'),
 ]
--- pizzashopapp/views.py
+++ pizzashopapp/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required  # проверка входа
 
 
 def home(request):
-    return render(request, 'pizzashopapp/home.html', {})
+    return redirect(pizzashop_home)
+
+
+@login_required(login_url='/pizzashop/sing-in/')
+def pizzashop_home(request):
+    return render(request, 'pizzashop/home.html', {})
+
+
+def pizzashop_sing_up(request):
+    return render(request, 'pizzashop/sing_up.html', {})
